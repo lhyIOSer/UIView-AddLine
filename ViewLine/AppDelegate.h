c0af6a5601da8c2c59303c3981f69fea346f59cb
lhy <lhy@lhy.local>
2016-05-25 15:31:03 +0800

    Initial Commit

---
+++ ViewLine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewLine
+//
+//  Created by lhy on 16/5/25.
+//  Copyright © 2016年 lhy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewLine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ViewLine
+//
+//  Created by lhy on 16/5/25.
+//  Copyright © 2016年 lhy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
